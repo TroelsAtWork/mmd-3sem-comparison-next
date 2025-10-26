b2dab75aa8ef437a783889243c1d9e205bda0487
TroelsAtWork <tras@kea.dk>
2025-10-26 11:11:35 +0100

    Add static like button

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,14 +1,7 @@
 "use client";
-import { useState } from "react";
 
 const LikeButton = () => {
-  const [likes, setLikes] = useState(0);
-  return (
-    <button onClick={() => setLikes(likes + 1)}>
-      {/* 🔥 {likes === 0 ? " Like" : likes === 1 ? ` Like ${likes}` : ` Likes ${likes}`} */}
-      🔥 {likes > 0 ? `Like${likes > 1 ? "s" : ""} ${likes}` : "Like"}
-    </button>
-  );
+  return <button>🔥 like</button>;
 };
 
 export default LikeButton;
